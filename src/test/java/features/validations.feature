d46938e7780f014d1a863f096c2b0d3867a4c17a
AndraAs <andra.astalus@gmail.com>
2021-01-30 13:52:20 +0200

    Added GET UserId API test

--- src/test/java/features/validations.feature
+++ src/test/java/features/validations.feature
@@ -2,7 +2,7 @@ Feature: Validating Title API's
 
 Scenario: Verify retrieving title is being succesfully returned using GetTitleAPI
 	Given GetTitle Api headers and query param
-#	When user calls GetTitle Api with "Get" http request
+	When user calls GetUserId Api with get http request
 #	Then the Api call is succesfull with "status code" "200"
 #	And title in response body is "rem alias distinctio quo quis"
 	
--- src/test/java/stepDefinitions/StepDefinition.java
+++ src/test/java/stepDefinitions/StepDefinition.java
@@ -1,28 +1,42 @@
 package stepDefinitions;
 
 import static io.restassured.RestAssured.given;
-
+import static org.hamcrest.Matchers.*;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.specification.RequestSpecification;
+
 import static org.junit.Assert.*;
 
 public class StepDefinition {
+	
+	int userId;
 	@Given("GetTitle Api headers and query param")
 	public void get_title_api_headers_and_query_param() {
 		RestAssured.baseURI = "https://jsonplaceholder.typicode.com/";
-		given().log().all().queryParam("title", "rem alias distinctio quo quis")
-		.header("Content-Type", "application/json").when().get("posts").then().log().all().assertThat().statusCode(200);
-	    
+		 String response =given().log().all().queryParam("title", "rem alias distinctio quo quis")
+		.header("Content-Type", "application/json").when().get("posts").then().assertThat().statusCode(200).body("[0].title", equalTo("rem alias distinctio quo quis"))
+	    .header("Content-Type", "application/json; charset=utf-8").extract().response().asString();
+		
+		
+		JsonPath js = new JsonPath(response);
+		String title =js.get("[0].title");
+		 userId = js.getInt("[0].userId");
+//		System.out.println("Title is " + title);
+//		System.out.println("user id is " + userId);
 	}
 
-//	@When("user calls GetTitle Api with {string} http request")
-//	public void user_calls_get_title_api_with_http_request(String string) {
-//		
-//	    throw new io.cucumber.java.PendingException();
-//	}
+	@When("user calls GetUserId Api with get http request")
+	public void user_calls_get_UserId_api_with_http_request() {
+		RestAssured.baseURI = "https://jsonplaceholder.typicode.com/";
+		 String responseUser =given().log().all().queryParam("userId", userId).header("Content-Type", "application/json").when().get("posts").then().assertThat().statusCode(200).body("[0].userId", equalTo(3))
+		    .header("Content-Type", "application/json; charset=utf-8").extract().response().asString();
+	    System.out.println("Response user is " + responseUser);
+	}
 //
 //	@Then("the Api call is succesfull with {string} {string}")
 //	public void the_api_call_is_succesfull_with(String string, String string2) {
