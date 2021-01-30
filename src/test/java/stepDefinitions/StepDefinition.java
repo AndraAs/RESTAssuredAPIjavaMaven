b96f785cfcaffdd3f9133a95a4a66a66f61a17cc
AndraAs <andra.astalus@gmail.com>
2021-01-30 19:28:22 +0200

    Added for loop for retrieving titles

--- src/test/java/stepDefinitions/StepDefinition.java
+++ src/test/java/stepDefinitions/StepDefinition.java
@@ -7,35 +7,50 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.restassured.RestAssured;
+import io.restassured.common.mapper.TypeRef;
 import io.restassured.path.json.JsonPath;
 import io.restassured.specification.RequestSpecification;
+import junit.framework.Assert;
 
 import static org.junit.Assert.*;
+
+import java.util.List;
+import java.util.Map;
+
+import files.ReusableMethods;
 
 public class StepDefinition {
 	
 	int userId;
 	@Given("GetTitle Api headers and query param")
+	
 	public void get_title_api_headers_and_query_param() {
+		String expectedTitle = "rem alias distinctio quo quis";
 		RestAssured.baseURI = "https://jsonplaceholder.typicode.com/";
-		 String response =given().log().all().queryParam("title", "rem alias distinctio quo quis")
-		.header("Content-Type", "application/json").when().get("posts").then().assertThat().statusCode(200).body("[0].title", equalTo("rem alias distinctio quo quis"))
+		 String response =given().log().all().queryParam("title", expectedTitle)
+		.header("Content-Type", "application/json").when().get("posts").then().assertThat().statusCode(200).body("[0].title", equalTo(expectedTitle))
 	    .header("Content-Type", "application/json; charset=utf-8").extract().response().asString();
 		
 		
-		JsonPath js = new JsonPath(response);
-		String title =js.get("[0].title");
+		JsonPath js =ReusableMethods.rawToJson(response);
+		String actualtTitle =js.get("[0].title");
+		Assert.assertEquals(expectedTitle, actualtTitle);
 		 userId = js.getInt("[0].userId");
-//		System.out.println("Title is " + title);
-//		System.out.println("user id is " + userId);
 	}
 
 	@When("user calls GetUserId Api with get http request")
 	public void user_calls_get_UserId_api_with_http_request() {
+		List<Map<String,Object>> responseUser = null;
 		RestAssured.baseURI = "https://jsonplaceholder.typicode.com/";
-		 String responseUser =given().log().all().queryParam("userId", userId).header("Content-Type", "application/json").when().get("posts").then().assertThat().statusCode(200).body("[0].userId", equalTo(3))
-		    .header("Content-Type", "application/json; charset=utf-8").extract().response().asString();
-	    System.out.println("Response user is " + responseUser);
+		  responseUser =given().log().all().queryParam("userId", userId).header("Content-Type", "application/json").when().get("posts").then().assertThat().statusCode(200).body("[0].userId", equalTo(userId))
+		    .header("Content-Type", "application/json; charset=utf-8").extract().response().as(new TypeRef<List<Map<String,Object>>>() {});
+
+	    for(Map<String,Object> posts : responseUser)
+		{
+	    	
+			System.out.println(posts.get("title"));
+		}
+	    System.out.println("Total posts from this author are : "+ responseUser.size());
 	}
 //
 //	@Then("the Api call is succesfull with {string} {string}")
