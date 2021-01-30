0d3a53cf05170039bbe721db60bf2e20c31be040
AndraAs <andra.astalus@gmail.com>
2021-01-30 23:21:52 +0200

    Added cucumber reporting

--- src/test/java/features/validations.feature
+++ src/test/java/features/validations.feature
@@ -1,8 +1,7 @@
 Feature: Validating Title API's
 
 Scenario: Verify retrieving title is being succesfully returned using GetTitleAPI
-	Given GetTitle Api headers and query param
+	Given GetTitle Api headers and "rem alias distinctio quo quis" query param
 	When user calls GetUserId Api with get http request
-#	Then the Api call is succesfull with "status code" "200"
-#	And title in response body is "rem alias distinctio quo quis"
+	Then all the posts returned have the same author
 	
--- src/test/java/stepDefinitions/StepDefinition.java
+++ src/test/java/stepDefinitions/StepDefinition.java
@@ -14,56 +14,64 @@ import junit.framework.Assert;
 
 import static org.junit.Assert.*;
 
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import files.ReusableMethods;
 
 public class StepDefinition {
-	
+	List<Map<String, Object>> responseUser = null;
 	int userId;
-	@Given("GetTitle Api headers and query param")
-	
-	public void get_title_api_headers_and_query_param() {
-		String expectedTitle = "rem alias distinctio quo quis";
+
+	@Given("GetTitle Api headers and {string} query param")
+
+	public void get_title_api_headers_and_query_param(String expectedTitle) {
 		RestAssured.baseURI = "https://jsonplaceholder.typicode.com/";
-		 String response =given().log().all().queryParam("title", expectedTitle)
-		.header("Content-Type", "application/json").when().get("posts").then().assertThat().statusCode(200).body("[0].title", equalTo(expectedTitle))
-	    .header("Content-Type", "application/json; charset=utf-8").extract().response().asString();
-		
-		
-		JsonPath js =ReusableMethods.rawToJson(response);
-		String actualtTitle =js.get("[0].title");
+		String response = given().queryParam("title", expectedTitle)
+				.header("Content-Type", "application/json").when().get("posts").then().assertThat().statusCode(200)
+				.body("[0].title", equalTo(expectedTitle)).header("Content-Type", "application/json; charset=utf-8")
+				.extract().response().asString();
+
+		JsonPath js = ReusableMethods.rawToJson(response);
+		String actualtTitle = js.get("[0].title");
 		Assert.assertEquals(expectedTitle, actualtTitle);
-		 userId = js.getInt("[0].userId");
+		userId = js.getInt("[0].userId");
 	}
 
 	@When("user calls GetUserId Api with get http request")
 	public void user_calls_get_UserId_api_with_http_request() {
-		List<Map<String,Object>> responseUser = null;
+
 		RestAssured.baseURI = "https://jsonplaceholder.typicode.com/";
-		  responseUser =given().log().all().queryParam("userId", userId).header("Content-Type", "application/json").when().get("posts").then().assertThat().statusCode(200).body("[0].userId", equalTo(userId))
-		    .header("Content-Type", "application/json; charset=utf-8").extract().response().as(new TypeRef<List<Map<String,Object>>>() {});
+		responseUser = given().queryParam("userId", userId).header("Content-Type", "application/json")
+				.when().get("posts").then().assertThat().statusCode(200).body("[0].userId", equalTo(userId))
+				.header("Content-Type", "application/json; charset=utf-8").extract().response()
+				.as(new TypeRef<List<Map<String, Object>>>() {
+				});
 
-	    for(Map<String,Object> posts : responseUser)
-		{
-	    	
+		for (Map<String, Object> posts : responseUser) {
+
 			System.out.println(posts.get("title"));
-		}
-	    System.out.println("Total posts from this author are : "+ responseUser.size());
-	}
-//
-//	@Then("the Api call is succesfull with {string} {string}")
-//	public void the_api_call_is_succesfull_with(String string, String string2) {
-//	    // Write code here that turns the phrase above into concrete actions
-//	    throw new io.cucumber.java.PendingException();
-//	}
-//
-//	@And("title in response body is {string}")
-//	public void title_in_response_body_is(String string) {
-//	    // Write code here that turns the phrase above into concrete actions
-//	    throw new io.cucumber.java.PendingException();
-//	}
 
+		}
+		System.out.println("Total posts from this author are : " + responseUser.size());
+	}
+
+	@Then("all the posts returned have the same author")
+	public void all_the_posts_returned_have_the_same_author() {
+		int expectedUserId = userId;
+		RestAssured.baseURI = "https://jsonplaceholder.typicode.com/";
+		responseUser = given().queryParam("userId", userId).header("Content-Type", "application/json")
+				.when().get("posts").then().assertThat().statusCode(200).body("[0].userId", equalTo(userId))
+				.header("Content-Type", "application/json; charset=utf-8").extract().response()
+				.as(new TypeRef<List<Map<String, Object>>>() {
+				});
+
+		for (Map<String, Object> posts : responseUser) {
+			Assert.assertEquals(expectedUserId, posts.get("userId"));
+
+		}
+	}
 
 }
